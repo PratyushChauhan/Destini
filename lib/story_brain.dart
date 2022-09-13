7577882e936ed40a2f70a81e4100ebbe93ccb3f5
Pratyush Chauhan <pratyushchauhan62@gmail.com>
2022-09-13 14:16:13 +0530

    Added the storyline, set button visibility.

--- lib/story_brain.dart
+++ lib/story_brain.dart
@@ -1,9 +1,11 @@
 import 'story.dart';
 
 class StoryBrain {
+  int _storyNumber = 0;
+
   List<Story> _storyData = [
     Story(
-        'Your car has blown a tire on a winding road in the middle of nowhere with no cell phone reception. You decide to hitchhike. A rusty pickup truck rumbles to a stop next to you. A man with a wide brimmed hat with soulless eyes opens the passenger door for you and asks: "Need a ride, boy?".',
+        'Your car has blown a tire on a winding road in the middle of nowhere with no cell phone reception. You decide to hitchhike. A rusty pickup truck rumbl es to a stop next to you. A man with a wide brimmed hat with soulless eyes opens the passenger door for you and asks: "Need a ride, boy?".',
         'I\'ll hop in. Thanks for the help!',
         'Better ask him if he\'s a murderer first.'),
     Story(
@@ -27,27 +29,57 @@ class StoryBrain {
         'Restart',
         '')
   ];
+  String getStory() {
+    return _storyData[_storyNumber].storyTitle;
+  }
+
+  String getChoice1() {
+    return _storyData[_storyNumber].choice1;
+  }
+
+  String getChoice2() {
+    return _storyData[_storyNumber].choice2;
+  }
+
+  void nextStory(int choiceNumber) {
+    if (choiceNumber == 1) {
+      //for choice == 1
+      if (_storyNumber == 0) {
+        _storyNumber = 2;
+      } else if (_storyNumber == 1) {
+        _storyNumber = 2;
+      } else if (_storyNumber == 2) {
+        _storyNumber = 5;
+      } else if ((_storyNumber == 3) |
+          (_storyNumber == 4) |
+          (_storyNumber == 5)) {
+        restart();
+      }
+    } else {
+      // for choice == 2
+      if (_storyNumber == 0) {
+        _storyNumber = 1;
+      } else if (_storyNumber == 1) {
+        _storyNumber = 3;
+      } else if (_storyNumber == 2) {
+        _storyNumber = 4;
+      } else if ((_storyNumber == 3) |
+          (_storyNumber == 4) |
+          (_storyNumber == 5)) {
+        restart();
+      }
+    }
+  }
+
+  void restart() {
+    _storyNumber = 0;
+  }
+
+  bool buttonShouldBeVisible() {
+    bool isVisible = true;
+    if ((_storyNumber == 3) | (_storyNumber == 4) | (_storyNumber == 5)) {
+      isVisible = false;
+    }
+    return isVisible;
+  }
 }
-//TODO: Step 7 - Uncomment the lines below to include storyData as a private property in StoryBrain. Hint: You might need to change something in story.dart to make this work.
-
-//TODO: Step 23 - Use the storyNumber property inside getStory(), getChoice1() and getChoice2() so that it gets the updated story and choices rather than always just the first (0th) one.
-
-//TODO: Step 8 - Create a method called getStory() that returns the first storyTitle from _storyData.
-
-//TODO: Step 11 - Create a method called getChoice1() that returns the text for the first choice1 from _storyData.
-
-//TODO: Step 12 - Create a method called getChoice2() that returns the text for the first choice2 from _storyData.
-
-//TODO: Step 25 - Change the storyNumber property into a private property so that only story_brain.dart has access to it. You can do this by right clicking on the name (storyNumber) and selecting Refactor -> Rename to make the change across all the places where it's used.
-
-//TODO: Step 16 - Create a property called storyNumber which starts with a value of 0. This will be used to track which story the user is currently viewing.
-
-//TODO: Step 17 - Create a method called nextStory(), it should not have any outputs but it should have 1 input called choiceNumber which will be the choice number (int) made by the user.
-
-//TODO: Step 20 - Download the story plan here: https://drive.google.com/uc?export=download&id=1KU6EghkO9Hf2hRM0756xFHgNaZyGCou3
-
-//TODO: Step 21 - Using the story plan, update nextStory() to change the storyNumber depending on the choice made by the user. e.g. if choiceNumber was equal to 1 and the storyNumber is 0, the storyNumber should become 2.
-
-//TODO: Step 22 - In nextStory() if the storyNumber is equal to 3 or 4 or 5, that means it's the end of the game and it should call a method called restart() that resets the storyNumber to 0.
-
-//TODO: Step 27 - Create a method called buttonShouldBeVisible() which checks to see if storyNumber is 0 or 1 or 2 (when both buttons should show choices) and return true if that is the case, else it should return false.
